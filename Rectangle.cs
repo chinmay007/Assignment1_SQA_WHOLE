a9fcd344e6d7776868107758920a6d001b47a006
Chinmay Shah <Cshah5582@conestogac.on.ca>
2021-02-11 16:23:37 +0530

    Rectangle class completed

---
+++ Rectangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1_SQA
+{
+    public class Rectangle
+    {
+        private int width, length;
+        Rectangle()
+        {
+            this.width = 1;
+            this.length = 1;
+        }
+        Rectangle(int w,int h)
+        {
+            this.width = w;
+            this.length = h;
+        }
+        public int GetLength()
+        {
+            return this.length;     
+        }
+        public int SetLength(int length)
+        {
+            this.length = length;
+            return this.length;
+        }
+        public int GetWidth()
+        {
+            return this.width;
+        }
+        public int SetWidth(int width)
+        {
+            this.width = width;
+            return this.width;
+        }
+        public int GetPerimiter()
+        {
+            return (2*(length + width));
+        }
+        public int GetArea()
+        {
+            return (width * length);
+        }
+    }
+}
